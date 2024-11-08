dc45c6cdc4339ff6e0ab3e1b5120835470aa65d3
Jun Luo <4catcode@gmail.com>
2024-11-08 19:58:28 +0800

    improve stop process

--- health_check/main.py
+++ health_check/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
@@ -5,8 +6,11 @@ import httpx
 import os
 
 SOROBAN_RPC_URL = os.getenv('SOROBAN_RPC_URL', 'https://mainnet.sorobanrpc.com')
+STOPPING = False
 
 async def health(request):
+    if STOPPING:
+        return JSONResponse({"healthy": False}, status_code=503)
     try:
         async with httpx.AsyncClient() as client:
             response = await client.post(
@@ -40,8 +44,14 @@ async def health(request):
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
+async def on_shutdown():
+    STOPPING = True
+    print("Stopping the app, wait for 5 seconds")
+    await asyncio.sleep(5)
+    print("App stopped")
+
 
 app = Starlette(debug=True, routes=[
     Route('/', homepage),
     Route('/health', health),
-])
+], on_shutdown=[on_shutdown])
